83e5491fa1218d2c7dba7d9de8b8e0d905d77dc7
Omar Benmegdoul <omarbenmeg@gmail.com>
2021-05-28 11:21:43 -0400

    tasks for first ay evening

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,33 @@
 import React, { useState, useEffect } from "react";
-
+import { Switch, Route, useParams } from "react-router-dom";
 function App() {
-  return <div>hello world!</div>;
+  return (
+    <Switch>
+      <Route path={["/", "/:pageNum"]}>I'm a placeholder for 24-product-view. Page number: {useParams.pageNum}</Route>
+      {/*useParams.pageNum may be undefined if path is "/" */}
+      <Route exact path="/product/:productId">
+        I'm a placeholder for 1-product-view
+      </Route>
+      <Route exact path="/cart">
+        I'm a placeholder for the cart
+      </Route>
+      <Route exact path="/test1">
+        Use me as a sandbox
+      </Route>
+      <Route exact path="/test1">
+        Use me as a sandboxcd 
+      </Route>
+      <Route exact path="/test2">
+        Use me as a sandbox
+      </Route>
+      <Route exact path="/test3">
+        Use me as a sandbox
+      </Route>
+      <Route exact path="/test4">
+        Use me as a sandbox
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
