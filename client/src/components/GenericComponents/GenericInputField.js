7abb676a1b81495002af82f22e4f55e48c6bb54e
Pragas Velauthapillai <pragas_93@hotmail.com>
2021-05-28 12:42:29 -0400

    added generic input field

---
+++ client/src/components/GenericComponents/GenericInputField.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+import styled from "styled-components";
+
+const GenericInputField = ({ number }) => {
+  return (
+    <Form>
+      <label>
+        {number}
+        <input type="number" name="itemNumber" />
+      </label>
+      {/* not needed if cart button validates item num*/}
+      {/* <input type="submit" value="Submit" /> */}
+
+      {/* button for increment and decrement */}
+      {/* <button>+</button>  */}
+      {/* <button>-</button>  */}
+    </Form>
+  );
+};
+/*ref to point
+onclick increment decrement
+*/
+const Form = styled.div`
+  height: 50px;
+  width: 10px;
+  font-size: 16px;
+
+  /* font-family */
+
+  label {
+    display: none;
+  }
+
+  input {
+    border-radius: 3px;
+    border: 1px solid #e4e8eb;
+    box-sizing: border-box;
+    color: #464a5c;
+    font-size: 15px;
+    font-weight: 300;
+    height: 36px;
+    padding: 8px 12px 10px 12px;
+    width: 100%;
+
+    /* &::placeholder {
+      color: #999;
+    } */
+  }
+`;
+
+export default GenericInputField;
