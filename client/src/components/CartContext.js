436d85b7f0fd31a4e6f17521e5d28bfeaec88ea7
Omar Benmegdoul <omarbenmeg@gmail.com>
2021-05-30 14:34:00 -0400

    header fix, context fix

--- client/src/components/CartContext.js
+++ client/src/components/CartContext.js
@@ -1,22 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-export const CartContext = React.createContext({
-  cartContents: {
-    //notice that cartContents is not an array, unlike items.json
-    6543: {
-      name: "Barska GB12166 Fitness Watch with Heart Rate Monitor",
-      price: "$49.99",
-      body_location: "Wrist",
-      category: "Fitness",
-      imageSrc:
-        "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHB...<REST_OF_IMAGE_ENCODING>",
-      companyId: 19962,
-      numInStock: 9,
-      numInCart: 5,
-    },
-  },
-  cartDispatch: null,
-});
+export const CartContext = React.createContext({});
 
 const cartReducer = (state, action) => {
   console.log(`❗ CartContext.js:6 'state' <${typeof state}>`, state);
@@ -34,12 +18,14 @@ const cartReducer = (state, action) => {
     case "update":
       //undefined.numInCart already throws an error
       if (!action.newNumInCart && action.newNumInCart != 0) {
-        throw new Error(`${action.newNumInCart} is not a valid newNumInCart value`)
-      }
-      else if (!action.newNumInCart) {
+        throw new Error(
+          `${action.newNumInCart} is not a valid newNumInCart value`
+        );
+      } else if (!action.newNumInCart) {
         delete state[action.itemId];
-      } 
-      else {state[action.itemId].numInCart = action.newNumInCart;}
+      } else {
+        state[action.itemId].numInCart = action.newNumInCart;
+      }
 
       break;
     default:
@@ -48,7 +34,20 @@ const cartReducer = (state, action) => {
   console.log(`❗ CartContext.js:16 'state' <${typeof state}>`, state);
 };
 export const CartContextProvider = ({ children }) => {
-  const [cartContents, cartDispatch] = React.useReducer(cartReducer, {});
+  const [cartContents, cartDispatch] = React.useReducer(cartReducer, {
+    //notice that cartContents is not an array, unlike items.json
+    6543: {
+      name: "Barska GB12166 Fitness Watch with Heart Rate Monitor",
+      price: "$49.99",
+      body_location: "Wrist",
+      category: "Fitness",
+      imageSrc:
+        "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHB...<REST_OF_IMAGE_ENCODING>",
+      companyId: 19962,
+      numInStock: 9,
+      numInCart: 5,
+    },
+  });
   return (
     <CartContext.Provider
       value={{ cartContents: cartContents, cartDispatch: cartDispatch }}
--- client/src/components/SiteHeader.js
+++ client/src/components/SiteHeader.js
@@ -4,17 +4,18 @@ import { Link } from "react-router-dom";
 import { CartContext } from "./CartContext";
 
 const SiteHeader = ({}) => {
-  const cartProductCount = Object.keys(React.useContext(CartContext)).length;
+
+  const cartProductCount = Object.keys(React.useContext(CartContext).cartContents).length
 
   return (
     <Wrapper>
       <Link to="/">
-        <h1>don't forget to push it</h1>
+        <h1>don't forget to push it // git gud</h1>
       </Link>
-      <Nav
+      <Nav cartProductCount={cartProductCount} 
       >
         <Link to="/cart">
-          <h2 className={cartProductCount ? "display-product-count" : ""}>cart</h2>
+          <h2 className={cartProductCount ? "display-product-count" : ""} >cart</h2>
         </Link>
       </Nav>
     </Wrapper>
@@ -33,15 +34,24 @@ const Nav = styled.nav`
   flex-direction: row;
   justify-content: space-between;
   padding: 20px;
+  & .display-product-count {
+    position:relative;
+  }
   & .display-product-count::after {
     content: "${(props)=>{return props.cartProductCount}}";
+    min-height:1.2em;
+    min-width:1.2em;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    font-size:0.8em;
     background-color:#E81140;
     border-radius:9999px;
-    padding:5px;
     position:absolute;
-    top:-5px;
-    left:calc(100% + 5px);
+    top:-10px;
+    left:calc(100% - 5px);
     z-index:1;
+    color:#fff;
   }
 `;
 
