a36da0cb4ccfa4cf09ce35fe107a2e664f16468a
Pragas Velauthapillai <pragas_93@hotmail.com>
2021-05-31 19:04:25 -0400

    add itemdetail thumbnail

---
+++ client/src/components/Item/ItemThumbnail.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const ItemThumbnail = styled.img`
+
+  height: 352px;
+  width: 352px;
+  background-size: cover;
+  overflow: hidden;
+  display: block;
+  margin: 30px auto 0px auto;
+  /* width: 50%; */
+  }
+`;
+
+export default ItemThumbnail;
