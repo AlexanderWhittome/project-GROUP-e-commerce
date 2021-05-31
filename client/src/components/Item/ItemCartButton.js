8b1a89ed5a9c5c1970d5a705e4ea58143855aafe
Pragas Velauthapillai <pragas_93@hotmail.com>
2021-05-31 19:04:54 -0400

    add itemdetail cart button

---
+++ client/src/components/Item/ItemCartButton.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const CartButton = styled.button`
+  position: relative;
+  background: blue;
+  border-color: transparent;
+  border-radius: 13px 13px 13px 13px;
+  color: #fff;
+  cursor: pointer;
+  display: block;
+  font-family: "Poppins", Arial, Helvetica, sans-serif;
+  font-size: 15px;
+  font-weight: 600;
+  width: 15%;
+`;
