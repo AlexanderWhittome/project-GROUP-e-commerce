3cb8a524b53e2226104fbcd9c668b0e9453b9c44
Pragas Velauthapillai <pragas_93@hotmail.com>
2021-06-01 15:04:58 -0400

    add space

--- client/src/components/Item/CompanyInfo.js
+++ client/src/components/Item/CompanyInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+
 const CompanyInfo = (prop) => {
   const [company, getCompany] = React.useState({});
   const [isLoading, setisLoading] = React.useState(true);
